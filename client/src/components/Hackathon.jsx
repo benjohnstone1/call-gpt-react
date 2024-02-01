6e593ae5e72f6eefac009dd549edbaea7a791184
Brent Bailey <bailey.brent@gmail.com>
2024-02-01 02:37:46 -0500

    Added Paste Design Elements

--- client/src/components/Hackathon.jsx
+++ client/src/components/Hackathon.jsx
@@ -30,7 +30,7 @@ const Hackathon = () => {
         "You are a customer support representative for Nike. You have a youthful and cheery personality. Keep your responses as brief as possible but make every attempt to keep the caller on the phone without being rude. Don't ask more than 1 question at a time. Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous. Speak out all prices to include the currency. Please help them decide between the Vaporfly, Air Max and Pegasus by asking questions like 'Do you prefer shoes that are for racing or for training?'. If they are trying to choose between the vaporfly and pegasus try asking them if they need a high mileage shoe. Once you know which shoe they would like ask them what size they would like to purchase and try to get them to place an order. You must add a '•' symbol every 5 to 10 words at natural pauses where your response can be split for text to speech."
       );
       setInitialGreeting(
-        "Hi, and thanks for calling Nike! I understand you're looking for a pair of Air Jordans, is that correct?"
+        "Hi, and thanks for calling Nike! I understand you're looking for a pair of Nike Shoes, is that correct?"
       );
       setLanguageSettings("English & French");
     } else {
